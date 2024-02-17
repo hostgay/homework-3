6368b126f2b11ca70e1d14df31265f4811a327ef
hostgay <bektur.tilenov@mail.ru>
2024-02-17 12:15:05 +0300

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount {
+    private double amount;
+
+    public BankAccount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        amount += sum;
+    }
+
+    public void withDraw(int sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Not enough funds on the account", amount);
+        } else {
+            amount -= sum;
+        }
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(15000);
+
+        try {
+            while (true) {
+                account.withDraw(6000);
+            }
+        } catch (LimitException e) {
+            System.out.println(e.getMessage() + ", remaining amount: " + e.getRemainingAmount());
+        }
+
+        account.getInfo();
+    }
+}
